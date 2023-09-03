fc6543a6a15661245e65b969ddd5c890a70a92f6
suzannah thompson <suzi@suzannahs-mbp.home>
2023-09-03 16:34:13 +0100

    Add port to index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const app = express();
+
+// mongoose.Promise = global.Promise;
+// mongoose.set("strictQuery", false);
+// // new code below
+// // mongoose.set("useCreateIndex", true);
+// //new code above
+// mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+//   useNewUrlParser: true,
+// });
+
+app.use(bodyParser.json());
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`API running on port ${PORT}`);
