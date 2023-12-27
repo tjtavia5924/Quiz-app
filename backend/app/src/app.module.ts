b7c6e2a43585030a11cc7e9f25053326014a3a52
Suzannah T <suzannahthompson@hotmail.co.uk>
2023-12-27 15:42:20 +0000

    added mongoose package, and updated app.module.ts

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import userModel from './users/Schema/userschema';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    MongooseModule.forRoot('mongodb+srv://mongo:<password>@cluster1.n5qimr9.mongodb.net/',{dbName: 'tester'}),
+    MongooseModule.forFeature([{ name: 'user', schema: userModel }]),],
   controllers: [AppController],
   providers: [AppService],
 })
